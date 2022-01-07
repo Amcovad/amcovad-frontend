daa34bce15705a7c88e3cd6f948c3a032b98af9f
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-01-07 00:52:19 +0000

    created a button component

---
+++ components/buttons/Outlinebtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Outlinebtn() {
+  return (
+    <div>
+      <button className="h-10 px-5 m-2 text-[#5D6677] uppercase transition-colors duration-150 border border-[#5D6677] rounded-md focus:shadow-outline hover:bg-[#01A1DF] hover:border-[#01A1DF] hover:text-white">
+        sign in
+      </button>
+    </div>
+  );
+}
+
+export default Outlinebtn;
---
+++ components/buttons/Secondarybtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Secondarybtn = () => {
+  return (
+    <div>
+      <button className="h-10 px-5 m-2 text-white transition-colors uppercase duration-150 bg-[#01A1DF] rounded-md focus:shadow-outline hover:bg-[#5D6677] hover:text-white">
+        try it now
+      </button>
+    </div>
+  );
+};
+
+export default Secondarybtn;
