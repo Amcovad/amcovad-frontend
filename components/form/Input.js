b052382ffeb300a04ade97525a3a81bda8d78917
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-02-02 23:16:22 +0000

    Modify css for the sign in page

--- components/form/Input.js
+++ components/form/Input.js
@@ -16,7 +16,6 @@ const Input = ({ className, Icon, label, name, placeholder, type }) => {
         htmlFor={name}
         className={`absolute text-sm text-amcovad-secondary-700 duration-300 transform -translate-y-7 scale-75 top-3 left-1 z-10 origin-[0] peer-focus:left-1 peer-focus:text-amcovad-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7`}
       >
-        {/* absolute text-sm text-amcovad-secondary-700 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-3 left-1 z-10 origin-[0] peer-focus:left-1 peer-focus:text-amcovad-primary-500 peer-focus:dark:text-amcovad-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 */}
         {label}
       </label>
       {Icon && (
