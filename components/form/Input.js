92942bedf979f13785a581720b22d2f9e47b98b6
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-02-02 01:32:28 +0000

    Initial commit of sign-in page

--- components/form/Input.js
+++ components/form/Input.js
@@ -3,29 +3,27 @@ import PropTypes from 'prop-types';
 
 const Input = ({ className, Icon, label, name, placeholder, type }) => {
   return (
- 
-      <div className="flex relative">
-        <input
-          className={`h-12 outline-none px-2 w-full bg-[#D6D9DD] mt-8 rounded-md pr-8 border border-[#D6D9DD] transition-all ${className}`}
-          name={name}
-          placeholder={label ? null : placeholder}
-          type={type}
-          required
-          id={name}
-        />
-        <label
-          htmlFor={name}
-          className="text-sm absolute top-11 text-[#344055] left-2 pointer-events-none transition-all"
-        >
-          {label}
-        </label>
-        {Icon && (
-          <span className="absolute top-12 right-2 cursor-pointer" data-testid="icon">
-            {Icon}
-          </span>
-        )}
-      </div>
-
+    <div className="flex relative">
+      <input
+        className={`h-12 outline-none px-2 w-full bg-[#D6D9DD] mt-8 rounded-md pr-8 border border-[#D6D9DD] transition-all ${className}`}
+        name={name}
+        placeholder={label ? null : placeholder}
+        type={type}
+        required
+        id={name}
+      />
+      <label
+        htmlFor={name}
+        className="text-sm absolute top-11 text-[#344055] left-2 pointer-events-none transition-all"
+      >
+        {label}
+      </label>
+      {Icon && (
+        <span className="absolute top-12 right-2 cursor-pointer" data-testid="icon">
+          {Icon}
+        </span>
+      )}
+    </div>
   );
 };
 Input.propTypes = {
