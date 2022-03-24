059f94d462da61cfba6d242d01e31dfa6d71190a
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-03-24 21:07:40 +0100

    #181656720 Add carousel to hompage

--- components/sections/HowItWork.js
+++ components/sections/HowItWork.js
@@ -1,43 +1,101 @@
 import React from 'react';
 import Image from 'next/image';
-import Phone from '../../public/assets/hero/phone.png';
+import Slider from 'react-slick';
+import slideData from '../../data/Hero/slide';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-const HowItWork = () => {
+export const Slide = ({ count, image, orderLast, title, text }) => {
   return (
-    <section className="text-amcovad-secondary-300 bg-amcovad-secondary-100 lg:bg-gradient-section ">
-      <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20">
-        <div className="grid gap-5 row-gap-8 lg:grid-cols-2 lg">
-          <div className="lg:p-12 xl:p-0">
-            <div className="w-full relative pb-6 my-20 sm:pr-5 lg:pr-0 md:pb-0">
-              <div className="px-2 max-w-[300.96px] md:max-w-[400px] lg:max-w-[387px]">
-                <h1 className=" pb-8 lg:pb-12  text-4xl font-RozhaOne  font-extrabold tracking-tight text-amcovad-secondary-600 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                  <span className="block">
-                    How <span className="text-amcovad-primary-500">Amcovad </span>secure your transactions.
-                  </span>
-                </h1>
+    <div className="lg:pt-32 bg-transparent">
+      <div className="container m-auto px-6 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-2 lg:items-center lg:gap-12 xl:gap-32 ">
+          <div
+            className={classNames('w-full lg:w-6/12 xl:pl-12 2xl:pl-32 mt-20 lg:mt-0 ', { 'order-last': orderLast })}
+          >
+            <div className="pt-1 lg:pt-0 lg:px-10 xl:p-0 ">
+              <div className="w-full relative my-20 md:pl-10 lg:pl-0 sm:pr-5 lg:pr-0 md:pb-0">
+                <div className="px-2 max-w-[300.96px] md:max-w-[400px] lg:max-w-[387px]">
+                  <h1 className=" pb-8 lg:pb-12 text-4xl font-RozhaOne font-normal tracking-tight text-amcovad-secondary-600 sm:text-5xl md:text-4xl lg:text-5xl xl:text-[52px]">
+                    <span className="block">
+                      How <span className="text-amcovad-primary-500">Amcovad </span>secure your transactions.
+                    </span>
+                  </h1>
+                </div>
+                <p className="text-amcovad-secondary-700 z-10 pl-16 pb-4 font-Inter text-[13.6833px] md:text-sm lg:text-base font-[700] mx-auto sm:max-w-md  md:max-w-3xl">
+                  {title}
+                </p>
+                <p className="max-w-[21.25rem] md:max-w-[30rem] lg:max-w-[23.6875rem] z-10 text-[13.6833px] md:text-sm lg:text-base  text-amcovad-secondary-600 font-Inter font-normal">
+                  {text}
+                </p>
+                <span className="font-Inter block text-[14.875rem] md:text-[12.5rem] lg:text-[16rem] left-[-1.3125rem] md:left-[15px] lg:left-[-1.625rem] top-[3rem] md:top-[1.5rem] lg:top-20 xl:top-24 absolute font-normal text-amcovad-black opacity-10">
+                  {count}
+                </span>
               </div>
-
-              <p className="text-amcovad-secondary-700 z-10 pl-16 pb-4 font-Inter text-[13.6833px] md:text-sm lg:text-base font-[700] mx-auto sm:max-w-md  md:max-w-3xl">
-                Setup your account
-              </p>
-              <p className="max-w-[20.25rem] md:max-w-[27rem] lg:max-w-[23.6875rem] z-10 text-[13.6833px] md:text-sm lg:text-base  text-amcovad-secondary-600 font-Inter font-normal">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum et accumsan nulla posuere aliquet ac
-                eleifend erat viverra. Nisi maecenas laoreet aenean suscipit. Blandit at vulputate non nunc, mollis amet
-              </p>
-
-              <span className="font-['Inter'] block text-[15.5rem] md:text-[12.5rem] lg:text-[16rem] left-[-1.3125rem] md:left-[-0.625rem] lg:left-[-1.625rem] top-[3rem] md:top-[1.5rem] lg:top-20 xl:top-[7.6rem] absolute font-normal text-amcovad-black opacity-10">
-                1
-              </span>
             </div>
           </div>
-
-          <div className="hidden w-[75%] mx-auto lg:flex justify-center items-center pr-5 ">
-            <Image src={Phone} height="650" width="395" alt="phone image" />
+          <div className="hidden lg:block lg:w-6/12 px-auto lg:pl-20 xl:pl-28 2xl:pl-48  ">
+            <Image src={image} height="600" width="395" alt="slide image" />
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
+};
+
+const HowItWork = () => {
+  const slideSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: false
+  };
+  const slideSettingsMobile = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false
+  };
+  return (
+    <>
+      <section className="lg:hidden text-amcovad-secondary-300 h-[615px] md:h-[550px] lg:h-[850px] bg-amcovad-secondary-100 lg:bg-gradient-section overflow-hidden ">
+        <Slider {...slideSettingsMobile}>
+          {slideData.map(({ count, image, text, title }, index) => {
+            return <Slide key={index} count={count} image={image} title={title} text={text} />;
+          })}
+        </Slider>
+      </section>
+      <section className="hidden lg:block text-amcovad-secondary-300 h-[615px] md:h-[550px] lg:h-[850px] bg-amcovad-secondary-100 lg:bg-gradient-section overflow-hidden ">
+        <Slider {...slideSettings}>
+          {slideData.map(({ count, image, orderLast, title, text }, index) => {
+            return <Slide key={index} count={count} image={image} orderLast={orderLast} title={title} text={text} />;
+          })}
+        </Slider>
+      </section>
+    </>
+  );
+};
+
+HowItWork.propTypes = {
+  count: PropTypes.string,
+  image: PropTypes.any,
+  orderLast: PropTypes.bool,
+  text: PropTypes.string,
+  title: PropTypes.string
+};
+
+HowItWork.defaultProps = {
+  count: '1',
+  image: null,
+  orderLast: false,
+  text: null,
+  title: null
 };
 
 export default HowItWork;
---
+++ data/Hero/slide.js
@@ -0,0 +1,24 @@
+const slideData = [
+  {
+    count: '1',
+    image: '/assets/hero/phone.png',
+    orderLast: false,
+    title: 'Setup your account',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum et accumsan nulla posuere aliquet ac eleifend erat viverra. Nisi maecenas laoreet aenean suscipit. Blandit at vulputate nonnunc, mollis amet'
+  },
+  {
+    count: '2',
+    image: '/assets/hero/phone.png',
+    orderLast: true,
+    title: 'Setup your account',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum et accumsan nulla posuere aliquet ac eleifend erat viverra. Nisi maecenas laoreet aenean suscipit. Blandit at vulputate nonnunc, mollis amet'
+  },
+  {
+    count: '3',
+    image: '/assets/hero/phone.png',
+    orderLast: false,
+    title: 'Setup your account',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum et accumsan nulla posuere aliquet ac eleifend erat viverra. Nisi maecenas laoreet aenean suscipit. Blandit at vulputate nonnunc, mollis amet'
+  }
+];
+export default slideData;
