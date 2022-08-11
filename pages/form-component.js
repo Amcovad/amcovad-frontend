9bb56bb0254dc043165e42a2158e8a2ca4aeef52
Adeyemo Rasheed <71451975+RACHID-WEBDEV@users.noreply.github.com>
2022-08-11 11:24:13 +0100

    #182507028 Ft Modified Form Components Design  (#43)
    * Modified all colors design system
    
    * #182507028 Make changes to the form components design system
    
    * Modified existing form components
    
    * Modified badge components
    
    * made changes to the form components
    
    * #182507028 Made changes to the form components
    
    * #182507028 Modified changes to the existing form components

--- pages/form-component.js
+++ pages/form-component.js
@@ -7,6 +7,9 @@ import Select from '@/components/form/Select';
 import RadioGroup from '@/components/form/RadioGroup';
 import HookForm from '@/components/form/Form';
 import { LogoBlack, SignInImage } from '/public/assets/signUp';
+import { EmailIcon, TestIcon, TestIcon1, TestIcon2 } from '@/public/assets/dashboard/navBarIcon';
+import { Badge, BadgeGroup } from '@/components/form/Badge';
+import UserPhoto from '@/public/assets/dashboard/User2.png';
 
 const SignIn = () => {
   const FormSchema = yup
@@ -57,21 +60,85 @@ const SignIn = () => {
                       </div>
                       <div className="mt-8">
                         <HookForm onSubmit={onSubmit} schema={FormSchema}>
-                          <Input label="Email address" name="email" type="email" />
-                          <Input label="Password" name="password" type="password" />
+                          <Input
+                            label="Email address"
+                            name="email"
+                            type="email"
+                            leadingIcon={<EmailIcon />}
+                            hintText="This is a hint text to help your email."
+                            hintIcon
+                            floatLabel
+                            feedBack="FEEDBACK.SUCCESS"
+                          />
+                          <Input
+                            label="Password"
+                            name="password"
+                            type="password"
+                            hintText="This is a hint text to help your password."
+                            feedBack="FEEDBACK.SUCCESS"
+                          />
 
                           <Select
-                            defaultOption="Chooose The best wiisely"
-                            label="Choose Select"
+                            floatLabel
+                            leadingIcon={<EmailIcon />}
+                            hintIcon
+                            hintText="This is a hint text to help user select state of origin."
+                            defaultOption="Select your State of Origin"
+                            label="State of Origin"
                             name="select"
                             options={sampleData}
+                            feedBack="FEEDBACK.SUCCESS"
                           />
                           <div className="my-4">
-                            <Toggle name="toggle" label="Click the Toggle" />
+                            <Toggle
+                              name="toggle"
+                              label="Remember me"
+                              helperLabel
+                              description="Save my login details for next time."
+                            />
                           </div>
-                          <div className=" mt-8 mb-3">
+                          <div className="my-4">
+                            <Toggle
+                              checked
+                              //disabled
+                              name="toggledisable"
+                              label="Remind me tomorrow"
+                              helperLabel
+                              description="Disabled details remind next time."
+                            />
+                          </div>
+                          <div className=" mt-4 mb-3">
                             <CheckboxGroup
+                              // size="md"
+                              disabled
+                              //checked
+                              name="acceptTerm"
+                              //helperLabel
+                              description="This is a checkbox description example"
+                              options={[
+                                {
+                                  label: (
+                                    <>
+                                      Disabled checked I have agree to the
+                                      <Link href="#" passHref>
+                                        <a className=" text-primary-400 hover:text-primary-500"> Terms of Service</a>
+                                      </Link>
+                                    </>
+                                  ),
+                                  value: true
+                                }
+                              ]}
+                            />
+                          </div>
+                          <div className=" mt-4 mb-3">
+                            <CheckboxGroup
+                              // minusIcon
+                              // size="md"
+                              // disabled
+                              // checked
                               name="acceptTerms"
+                              helperLabel
+                              description="This is a checkbox description"
                               options={[
                                 {
                                   label: (
@@ -96,15 +163,69 @@ const SignIn = () => {
                           </div>
 
                           <div className="my-4">
-                            <RadioGroup name="radio" options={sampleData} />
+                            <RadioGroup
+                              // checkIcon
+                              // size="md"
+                              //disabled
+                              //checked
+                              name="radio"
+                              // columns
+                              // helperLabel
+                              // description="This is a checkbox description"
+                              options={sampleData}
+                            />
                           </div>
-                          <Textarea label="Your message" name="message" className="bg-secondary-50" />
+                          <Textarea
+                            floatLabel
+                            leadingIcon={<EmailIcon />}
+                            hintIcon
+                            hintText="This is a hint text to help user Textarea."
+                            label="Your message"
+                            name="message"
+                            // feedBack="FEEDBACK.SUCCESS"
+                          />
+
+                          <div className="my-4">
+                            <Badge
+                              text="New feature"
+                              // color="purple"
+                            />
+                            <BadgeGroup
+                              badgeText="Badge feature"
+                              // avatar={UserPhoto}
+                              //afterIcon={<TestIcon width={12} height={12} />}
+                              // color="orange"
+                              text="We’ve just released a new feature"
+                            />
+                          </div>
 
                           <div>
-                            <Button className=" w-full font-semibold py-2.5 px-5 text-black">Set Up Amcovad UI</Button>
+                            <Button className="w-full text-center" size="md">
+                              Set Up Amcovad UI
+                            </Button>
                           </div>
                         </HookForm>
                       </div>
+                    </div>
+                    <div className="my-4 flex flex-wrap items-center bg-secondary-25 p-5">
+                      <Button size="xs">Button xs CTA</Button>
+                      <Button size="sm" leftIcon={<TestIcon2 />} outline color="danger">
+                        Button sm CTA
+                      </Button>
+                      <Button size="md" color="secondary" rightIcon={<TestIcon />}>
+                        Button md CTA
+                      </Button>
+                      <Button color="warning" leftIcon={<TestIcon />} size="lg">
+                        Button lg CTA
+                      </Button>
+                      <Button size="xl" outline color="success" rightIcon={<TestIcon2 />}>
+                        Button xl CTA
+                      </Button>
+                      <Button size="lg" color="success">
+                        <TestIcon />
+                      </Button>
+
+                      <Button size="sm" outline color="danger" icon={<TestIcon2 />}></Button>
                     </div>
                   </div>
                 </div>
